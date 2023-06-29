0e52fa7c8fbd2d26e3ada0e75ed5612a551bd7cf
Mohammad Kazemi <116251073+mohammadkazemy@users.noreply.github.com>
2023-06-29 13:59:36 +0330

    new stable version of ui

--- build-untitled-Desktop_Qt_6_3_1_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build-untitled-Desktop_Qt_6_3_1_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -12,11 +12,13 @@
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QCheckBox>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -26,89 +28,83 @@ class Ui_MainWindow
 public:
     QWidget *centralwidget;
     QWidget *widget;
-    QLabel *label_3;
+    QLabel *label_2;
+    QWidget *verticalLayoutWidget;
+    QVBoxLayout *verticalLayout;
+    QCheckBox *checkBox;
+    QPushButton *pushButton_2;
+    QWidget *verticalLayoutWidget_2;
+    QVBoxLayout *verticalLayout_2;
     QLineEdit *lineEdit;
     QLineEdit *lineEdit_2;
-    QPushButton *pushButton;
-    QPushButton *pushButton_2;
+    QWidget *horizontalLayoutWidget;
+    QHBoxLayout *horizontalLayout;
     QPushButton *pushButton_3;
+    QPushButton *pushButton;
+    QLabel *label_3;
     QLabel *label;
-    QCheckBox *checkBox;
-    QLabel *label_2;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(947, 755);
+        MainWindow->resize(800, 600);
         MainWindow->setStyleSheet(QString::fromUtf8(""));
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         widget = new QWidget(centralwidget);
         widget->setObjectName(QString::fromUtf8("widget"));
-        widget->setGeometry(QRect(60, 30, 731, 611));
-        widget->setStyleSheet(QString::fromUtf8("background-color:rgba(0, 0, 0, 255);"));
-        label_3 = new QLabel(widget);
-        label_3->setObjectName(QString::fromUtf8("label_3"));
-        label_3->setGeometry(QRect(290, 120, 131, 71));
+        widget->setGeometry(QRect(0, 0, 800, 600));
+        widget->setStyleSheet(QString::fromUtf8("background-color:rgb(0, 0, 0);"));
+        label_2 = new QLabel(widget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setGeometry(QRect(20, 10, 351, 71));
         QFont font;
-        font.setPointSize(25);
+        font.setPointSize(30);
         font.setBold(true);
-        label_3->setFont(font);
-        label_3->setStyleSheet(QString::fromUtf8("color:rgb(255,255,0)	;\n"
-""));
-        lineEdit = new QLineEdit(widget);
-        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
-        lineEdit->setGeometry(QRect(190, 240, 331, 40));
-        QFont font1;
-        font1.setPointSize(10);
-        lineEdit->setFont(font1);
-        lineEdit->setStyleSheet(QString::fromUtf8("bbackground-color:rgba(0, 0, 0, 0);\n"
-"border:none;\n"
-"border-bottom:2px solid rgba(254, 231, 21, 1);\n"
-"color:rgba(255, 255, 255, 255);\n"
-"padding-bottom:7px;"));
-        lineEdit_2 = new QLineEdit(widget);
-        lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
-        lineEdit_2->setGeometry(QRect(190, 331, 331, 40));
-        lineEdit_2->setFont(font1);
-        lineEdit_2->setStyleSheet(QString::fromUtf8("bbackground-color:rgba(0, 0, 0, 0);\n"
-"border:none;\n"
-"border-bottom:2px solid rgba(254, 231, 21, 1);\n"
-"color:rgba(255, 255, 255, 255);\n"
-"padding-bottom:7px;"));
-        lineEdit_2->setEchoMode(QLineEdit::Password);
-        pushButton = new QPushButton(widget);
-        pushButton->setObjectName(QString::fromUtf8("pushButton"));
-        pushButton->setGeometry(QRect(370, 430, 161, 40));
-        QFont font2;
-        font2.setPointSize(16);
-        font2.setBold(true);
-        pushButton->setFont(font2);
-        pushButton->setStyleSheet(QString::fromUtf8("QPushButton#pushButton{\n"
-"	background-color:rgba(255, 255, 0, 190);\n"
-"	color:rgba(0, 0, 0, 255);\n"
-"	border-radius:5px;\n"
+        font.setItalic(true);
+        label_2->setFont(font);
+        label_2->setStyleSheet(QString::fromUtf8("QLabel#label_2{\n"
+"color:rgb(255,255,0)	;\n"
 "}\n"
 "\n"
-"QPushButton#pushButton:hover{\n"
-"	background-color:rgb(246, 120, 29);\n"
-"\n"
-"}\n"
-"\n"
-"QPushButton#pushButton:pressed{\n"
-"	padding-left:5px;\n"
-"	padding-top:5px;\n"
-"	background-color:rgba(150,123, 111, 255);\n"
+"QLabel#label_2:hover{\n"
+"color: rgb(254, 208, 83);\n"
 "}"));
-        pushButton_2 = new QPushButton(widget);
+        verticalLayoutWidget = new QWidget(widget);
+        verticalLayoutWidget->setObjectName(QString::fromUtf8("verticalLayoutWidget"));
+        verticalLayoutWidget->setGeometry(QRect(580, 320, 161, 71));
+        verticalLayout = new QVBoxLayout(verticalLayoutWidget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        checkBox = new QCheckBox(verticalLayoutWidget);
+        checkBox->setObjectName(QString::fromUtf8("checkBox"));
+        QFont font1;
+        font1.setBold(true);
+        checkBox->setFont(font1);
+        checkBox->setStyleSheet(QString::fromUtf8("QCheckBox#checkBox {\n"
+"padding-left: 10px;\n"
+"border-radius: 5px;\n"
+"color: #fefefe;\n"
+"}\n"
+"\n"
+"QCheckBox#checkBox:hover {\n"
+"	color: rgb(241, 182, 88);\n"
+"}\n"
+"\n"
+"QCheckBox#checkBox:pressed {\n"
+"	color: rgb(85, 255, 255);\n"
+"}"));
+
+        verticalLayout->addWidget(checkBox);
+
+        pushButton_2 = new QPushButton(verticalLayoutWidget);
         pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
-        pushButton_2->setGeometry(QRect(560, 375, 135, 31));
-        QFont font3;
-        font3.setPointSize(8);
-        font3.setBold(true);
-        pushButton_2->setFont(font3);
+        QFont font2;
+        font2.setPointSize(8);
+        font2.setBold(true);
+        pushButton_2->setFont(font2);
         pushButton_2->setStyleSheet(QString::fromUtf8("QPushButton#pushButton_2{\n"
 "	background-color:rgba(255, 255, 0, 190);\n"
 "	color:rgba(0, 0, 0, 255);\n"
@@ -124,17 +120,85 @@ public:
 "	padding-left:5px;\n"
 "	padding-top:5px;\n"
 "	background-color:rgba(150,123, 111, 255);\n"
+"}\n"
+""));
+
+        verticalLayout->addWidget(pushButton_2);
+
+        verticalLayoutWidget_2 = new QWidget(widget);
+        verticalLayoutWidget_2->setObjectName(QString::fromUtf8("verticalLayoutWidget_2"));
+        verticalLayoutWidget_2->setGeometry(QRect(250, 220, 321, 171));
+        verticalLayout_2 = new QVBoxLayout(verticalLayoutWidget_2);
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        verticalLayout_2->setContentsMargins(0, 0, 0, 0);
+        lineEdit = new QLineEdit(verticalLayoutWidget_2);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+        QFont font3;
+        font3.setPointSize(10);
+        lineEdit->setFont(font3);
+        lineEdit->setStyleSheet(QString::fromUtf8("\n"
+"QLineEdit#lineEdit {\n"
+"\n"
+"background-color:rgba(0, 0, 0, 0);\n"
+"border:none;\n"
+"border-bottom:2px solid rgba(254, 231, 21, 1);\n"
+"color:rgba(255, 255, 255, 255);\n"
+"padding-bottom:7px;\n"
+"\n"
+"}\n"
+"\n"
+"QLineEdit#lineEdit:hover {\n"
+"\n"
+"	background-color: rgb(42, 46, 52);\n"
+"	border-style: solid;\n"
+"	border-color: rgb(245, 179, 1);\n"
+"	color: rgb(241, 182, 88);\n"
+"\n"
 "}"));
-        pushButton_3 = new QPushButton(widget);
+
+        verticalLayout_2->addWidget(lineEdit);
+
+        lineEdit_2 = new QLineEdit(verticalLayoutWidget_2);
+        lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
+        lineEdit_2->setFont(font3);
+        lineEdit_2->setStyleSheet(QString::fromUtf8("\n"
+"QLineEdit#lineEdit_2 {\n"
+"\n"
+"background-color:rgba(0, 0, 0, 0);\n"
+"border:none;\n"
+"border-bottom:2px solid rgba(254, 231, 21, 1);\n"
+"color:rgba(255, 255, 255, 255);\n"
+"padding-bottom:7px;\n"
+"\n"
+"}\n"
+"\n"
+"QLineEdit#lineEdit_2:hover {\n"
+"\n"
+"	background-color: rgb(42, 46, 52);\n"
+"	border-style: solid;\n"
+"	border-color: rgb(245, 179, 1);\n"
+"	color: rgb(241, 182, 88);\n"
+"\n"
+"}"));
+        lineEdit_2->setEchoMode(QLineEdit::Password);
+
+        verticalLayout_2->addWidget(lineEdit_2);
+
+        horizontalLayoutWidget = new QWidget(widget);
+        horizontalLayoutWidget->setObjectName(QString::fromUtf8("horizontalLayoutWidget"));
+        horizontalLayoutWidget->setGeometry(QRect(250, 390, 321, 80));
+        horizontalLayout = new QHBoxLayout(horizontalLayoutWidget);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        pushButton_3 = new QPushButton(horizontalLayoutWidget);
         pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
-        pushButton_3->setGeometry(QRect(190, 430, 161, 41));
         QFont font4;
         font4.setPointSize(15);
         font4.setBold(true);
         pushButton_3->setFont(font4);
         pushButton_3->setStyleSheet(QString::fromUtf8("QPushButton#pushButton_3{\n"
 "	background-color:rgba(255, 255, 0, 190);\n"
-"	color:rgba(0, 0, 0, 255);\n"
+"	color: black;\n"
 "	border-radius:5px;\n"
 "}\n"
 "\n"
@@ -148,37 +212,52 @@ public:
 "	padding-top:5px;\n"
 "	background-color:rgba(150,123, 111, 255);\n"
 "}"));
-        label = new QLabel(widget);
-        label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(550, 10, 151, 171));
-        label->setStyleSheet(QString::fromUtf8("image: url(:/icons/telegramlogo.jpeg);"));
-        checkBox = new QCheckBox(widget);
-        checkBox->setObjectName(QString::fromUtf8("checkBox"));
-        checkBox->setGeometry(QRect(560, 335, 135, 31));
+
+        horizontalLayout->addWidget(pushButton_3);
+
+        pushButton = new QPushButton(horizontalLayoutWidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
         QFont font5;
+        font5.setPointSize(16);
         font5.setBold(true);
-        checkBox->setFont(font5);
-        checkBox->setStyleSheet(QString::fromUtf8("QCheckBox#checkBox{\n"
-"	background-color:rgba(255, 255, 0, 190);\n"
-"\n"
-"	color:rgba(0, 0, 0, 255);\n"
-"	border-radius:5px;\n"
+        pushButton->setFont(font5);
+        pushButton->setStyleSheet(QString::fromUtf8("QPushButton#pushButton {\n"
+"color:white;\n"
+"border-radius: 5px;\n"
+"Background: #393;\n"
+"color: #fefefe;\n"
 "}\n"
 "\n"
+"QPushButton#pushButton:hover {\n"
+"	background-color: rgb(42, 46, 52);\n"
+"border-style: solid;\n"
+" border-width: 1px;\n"
+"	color: rgb(241, 182, 88);\n"
+"}\n"
 "\n"
-"QCheckBox#checkBox:hover{\n"
-"	background-color:rgb(246, 120, 29);\n"
-"\n"
+"QPushButton#pushButton:pressed {\n"
+"border-style: solid;\n"
+"border-width: 1px;\n"
+"	background-color: rgb(25, 69, 105);\n"
+"	color: rgb(85, 255, 255);\n"
 "}"));
-        label_2 = new QLabel(widget);
-        label_2->setObjectName(QString::fromUtf8("label_2"));
-        label_2->setGeometry(QRect(10, 0, 291, 61));
+
+        horizontalLayout->addWidget(pushButton);
+
+        label_3 = new QLabel(widget);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+        label_3->setEnabled(true);
+        label_3->setGeometry(QRect(330, 120, 141, 71));
         QFont font6;
-        font6.setPointSize(25);
+        font6.setFamilies({QString::fromUtf8("Droid Sans")});
+        font6.setPointSize(30);
         font6.setBold(true);
-        font6.setItalic(true);
-        label_2->setFont(font6);
-        label_2->setStyleSheet(QString::fromUtf8("color:rgb(255,255,0)	;"));
+        label_3->setFont(font6);
+        label_3->setStyleSheet(QString::fromUtf8("color:rgb(255, 255, 0);"));
+        label = new QLabel(widget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(630, 10, 141, 151));
+        label->setStyleSheet(QString::fromUtf8("image: url(:/icons/telegramlogo.jpeg);"));
         MainWindow->setCentralWidget(centralwidget);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -192,15 +271,15 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
-        label_3->setText(QCoreApplication::translate("MainWindow", " Login", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "M2 messenger", nullptr));
+        checkBox->setText(QCoreApplication::translate("MainWindow", "Show password", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "Forgot password?", nullptr));
         lineEdit->setPlaceholderText(QCoreApplication::translate("MainWindow", " Username", nullptr));
         lineEdit_2->setPlaceholderText(QCoreApplication::translate("MainWindow", " Password", nullptr));
-        pushButton->setText(QCoreApplication::translate("MainWindow", "SIGN IN", nullptr));
-        pushButton_2->setText(QCoreApplication::translate("MainWindow", "Forgot password?", nullptr));
         pushButton_3->setText(QCoreApplication::translate("MainWindow", "Register", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "SIGN IN", nullptr));
+        label_3->setText(QCoreApplication::translate("MainWindow", "Login", nullptr));
         label->setText(QString());
-        checkBox->setText(QCoreApplication::translate("MainWindow", "Show password", nullptr));
-        label_2->setText(QCoreApplication::translate("MainWindow", "M2 messenger", nullptr));
     } // retranslateUi
 
 };
--- build-untitled-Desktop_Qt_6_3_1_MinGW_64_bit-Debug/ui_register.h
+++ build-untitled-Desktop_Qt_6_3_1_MinGW_64_bit-Debug/ui_register.h
@@ -12,6 +12,7 @@
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QCheckBox>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QLineEdit>
 #include <QtWidgets/QPushButton>
@@ -30,8 +31,6 @@ public:
     QLabel *Password_label;
     QLabel *ConfrimPassword_label;
     QLabel *PhoneNumber_label;
-    QPushButton *Submit_pushButton;
-    QPushButton *Back_pushButton;
     QLineEdit *FirstName_lineEdit;
     QLineEdit *LastName_lineEdit;
     QLineEdit *UserName_lineEdit;
@@ -40,17 +39,20 @@ public:
     QLineEdit *PhoneNumber_lineEdit;
     QLabel *label;
     QCheckBox *checkBox;
+    QWidget *horizontalLayoutWidget;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *Back_pushButton;
+    QPushButton *Submit_pushButton;
 
     void setupUi(QWidget *Register)
     {
         if (Register->objectName().isEmpty())
             Register->setObjectName(QString::fromUtf8("Register"));
-        Register->resize(796, 666);
+        Register->resize(800, 600);
         widget = new QWidget(Register);
         widget->setObjectName(QString::fromUtf8("widget"));
-        widget->setGeometry(QRect(0, 0, 801, 671));
-        widget->setStyleSheet(QString::fromUtf8("background-color:	rgb(0, 0, 0);\n"
-""));
+        widget->setGeometry(QRect(0, 0, 800, 600));
+        widget->setStyleSheet(QString::fromUtf8("background-color:black;"));
         label_2 = new QLabel(widget);
         label_2->setObjectName(QString::fromUtf8("label_2"));
         label_2->setGeometry(QRect(272, 50, 261, 81));
@@ -93,123 +95,219 @@ public:
         PhoneNumber_label->setGeometry(QRect(30, 440, 161, 30));
         PhoneNumber_label->setFont(font1);
         PhoneNumber_label->setStyleSheet(QString::fromUtf8("color:rgba(254, 231, 21, 1);"));
-        Submit_pushButton = new QPushButton(widget);
-        Submit_pushButton->setObjectName(QString::fromUtf8("Submit_pushButton"));
-        Submit_pushButton->setGeometry(QRect(360, 490, 181, 31));
-        QFont font2;
-        font2.setPointSize(15);
-        font2.setBold(true);
-        Submit_pushButton->setFont(font2);
-        Submit_pushButton->setStyleSheet(QString::fromUtf8("QPushButton#Submit_pushButton{\n"
-"	background-color:rgba(254, 231, 21, 1);\n"
-"	color:rgba(0, 0, 0, 255);\n"
-"	border-radius:5px;\n"
-"}\n"
-"\n"
-"QPushButton#Submit_pushButton:hover{\n"
-"	background-color:rgb(0, 255, 0);\n"
-"\n"
-"}\n"
-"\n"
-"QPushButton#Submit_pushButton:pressed{\n"
-"	padding-left:5px;\n"
-"	padding-top:5px;\n"
-"	background-color:rgba(150,123, 111, 255);\n"
-"}\n"
-"\n"
-"\n"
-""));
-        Back_pushButton = new QPushButton(widget);
-        Back_pushButton->setObjectName(QString::fromUtf8("Back_pushButton"));
-        Back_pushButton->setGeometry(QRect(150, 490, 171, 31));
-        Back_pushButton->setFont(font2);
-        Back_pushButton->setStyleSheet(QString::fromUtf8("QPushButton#Back_pushButton{\n"
-"	background-color:rgba(254, 231, 21, 1);\n"
-"	color:rgba(0, 0, 0, 255);\n"
-"	border-radius:5px;\n"
-"}\n"
-"\n"
-"QPushButton#Back_pushButton:hover{\n"
-"	background-color:rgb(255, 0, 0);\n"
-"}\n"
-"\n"
-"QPushButton#Back_pushButton:pressed{\n"
-"	padding-left:5px;\n"
-"	padding-top:5px;\n"
-"	background-color:rgba(150,123, 111, 255);\n"
-"}"));
         FirstName_lineEdit = new QLineEdit(widget);
         FirstName_lineEdit->setObjectName(QString::fromUtf8("FirstName_lineEdit"));
         FirstName_lineEdit->setGeometry(QRect(220, 185, 381, 28));
-        FirstName_lineEdit->setStyleSheet(QString::fromUtf8("bbackground-color:rgba(0, 0, 0, 0);\n"
+        FirstName_lineEdit->setStyleSheet(QString::fromUtf8("QLineEdit#FirstName_lineEdit {\n"
+"\n"
+"background-color:rgba(0, 0, 0, 0);\n"
 "border:none;\n"
 "border-bottom:2px solid rgba(254, 231, 21, 1);\n"
 "color:rgba(255, 255, 255, 255);\n"
-"padding-bottom:7px;"));
+"padding-bottom:7px;\n"
+"\n"
+"}\n"
+"\n"
+"QLineEdit#FirstName_lineEdit:hover {\n"
+"\n"
+"	background-color: rgb(42, 46, 52);\n"
+"	border-style: solid;\n"
+"	border-color: rgb(245, 179, 1);\n"
+"	color: rgb(241, 182, 88);\n"
+"\n"
+"}"));
         LastName_lineEdit = new QLineEdit(widget);
         LastName_lineEdit->setObjectName(QString::fromUtf8("LastName_lineEdit"));
         LastName_lineEdit->setGeometry(QRect(220, 230, 381, 28));
-        LastName_lineEdit->setStyleSheet(QString::fromUtf8("bbackground-color:rgba(0, 0, 0, 0);\n"
+        LastName_lineEdit->setStyleSheet(QString::fromUtf8("QLineEdit#LastName_lineEdit {\n"
+"\n"
+"background-color:rgba(0, 0, 0, 0);\n"
 "border:none;\n"
 "border-bottom:2px solid rgba(254, 231, 21, 1);\n"
 "color:rgba(255, 255, 255, 255);\n"
-"padding-bottom:7px;"));
+"padding-bottom:7px;\n"
+"\n"
+"}\n"
+"\n"
+"QLineEdit#LastName_lineEdit:hover {\n"
+"\n"
+"	background-color: rgb(42, 46, 52);\n"
+"	border-style: solid;\n"
+"	border-color: rgb(245, 179, 1);\n"
+"	color: rgb(241, 182, 88);\n"
+"\n"
+"}"));
         UserName_lineEdit = new QLineEdit(widget);
         UserName_lineEdit->setObjectName(QString::fromUtf8("UserName_lineEdit"));
         UserName_lineEdit->setGeometry(QRect(220, 280, 381, 28));
-        UserName_lineEdit->setStyleSheet(QString::fromUtf8("bbackground-color:rgba(0, 0, 0, 0);\n"
+        UserName_lineEdit->setStyleSheet(QString::fromUtf8("QLineEdit#UserName_lineEdit {\n"
+"\n"
+"background-color:rgba(0, 0, 0, 0);\n"
 "border:none;\n"
 "border-bottom:2px solid rgba(254, 231, 21, 1);\n"
 "color:rgba(255, 255, 255, 255);\n"
-"padding-bottom:7px;"));
+"padding-bottom:7px;\n"
+"\n"
+"}\n"
+"\n"
+"QLineEdit#UserName_lineEdit:hover {\n"
+"\n"
+"	background-color: rgb(42, 46, 52);\n"
+"	border-style: solid;\n"
+"	border-color: rgb(245, 179, 1);\n"
+"	color: rgb(241, 182, 88);\n"
+"\n"
+"}"));
         Password_lineEdit = new QLineEdit(widget);
         Password_lineEdit->setObjectName(QString::fromUtf8("Password_lineEdit"));
         Password_lineEdit->setGeometry(QRect(220, 330, 381, 28));
-        Password_lineEdit->setStyleSheet(QString::fromUtf8("bbackground-color:rgba(0, 0, 0, 0);\n"
+        Password_lineEdit->setStyleSheet(QString::fromUtf8("QLineEdit#Password_lineEdit {\n"
+"\n"
+"background-color:rgba(0, 0, 0, 0);\n"
 "border:none;\n"
 "border-bottom:2px solid rgba(254, 231, 21, 1);\n"
 "color:rgba(255, 255, 255, 255);\n"
-"padding-bottom:7px;"));
+"padding-bottom:7px;\n"
+"\n"
+"}\n"
+"\n"
+"QLineEdit#Password_lineEdit:hover {\n"
+"\n"
+"	background-color: rgb(42, 46, 52);\n"
+"	border-style: solid;\n"
+"	border-color: rgb(245, 179, 1);\n"
+"	color: rgb(241, 182, 88);\n"
+"\n"
+"}"));
         Password_lineEdit->setEchoMode(QLineEdit::Password);
         ConfrimPassword_lineEdit = new QLineEdit(widget);
         ConfrimPassword_lineEdit->setObjectName(QString::fromUtf8("ConfrimPassword_lineEdit"));
         ConfrimPassword_lineEdit->setGeometry(QRect(220, 378, 381, 28));
-        ConfrimPassword_lineEdit->setStyleSheet(QString::fromUtf8("bbackground-color:rgba(0, 0, 0, 0);\n"
+        ConfrimPassword_lineEdit->setStyleSheet(QString::fromUtf8("QLineEdit#ConfrimPassword_lineEdit {\n"
+"\n"
+"background-color:rgba(0, 0, 0, 0);\n"
 "border:none;\n"
 "border-bottom:2px solid rgba(254, 231, 21, 1);\n"
 "color:rgba(255, 255, 255, 255);\n"
-"padding-bottom:7px;"));
+"padding-bottom:7px;\n"
+"\n"
+"}\n"
+"\n"
+"QLineEdit#ConfrimPassword_lineEdit:hover {\n"
+"\n"
+"	background-color: rgb(42, 46, 52);\n"
+"	border-style: solid;\n"
+"	border-color: rgb(245, 179, 1);\n"
+"	color: rgb(241, 182, 88);\n"
+"\n"
+"}"));
         ConfrimPassword_lineEdit->setEchoMode(QLineEdit::Password);
         PhoneNumber_lineEdit = new QLineEdit(widget);
         PhoneNumber_lineEdit->setObjectName(QString::fromUtf8("PhoneNumber_lineEdit"));
         PhoneNumber_lineEdit->setGeometry(QRect(220, 430, 381, 28));
-        PhoneNumber_lineEdit->setStyleSheet(QString::fromUtf8("bbackground-color:rgba(0, 0, 0, 0);\n"
+        PhoneNumber_lineEdit->setStyleSheet(QString::fromUtf8("QLineEdit#PhoneNumber_lineEdit {\n"
+"\n"
+"background-color:rgba(0, 0, 0, 0);\n"
 "border:none;\n"
 "border-bottom:2px solid rgba(254, 231, 21, 1);\n"
 "color:rgba(255, 255, 255, 255);\n"
-"padding-bottom:7px;"));
+"padding-bottom:7px;\n"
+"\n"
+"}\n"
+"\n"
+"QLineEdit#PhoneNumber_lineEdit:hover {\n"
+"\n"
+"	background-color: rgb(42, 46, 52);\n"
+"	border-style: solid;\n"
+"	border-color: rgb(245, 179, 1);\n"
+"	color: rgb(241, 182, 88);\n"
+"\n"
+"}"));
         label = new QLabel(widget);
         label->setObjectName(QString::fromUtf8("label"));
         label->setGeometry(QRect(10, 10, 191, 161));
         label->setStyleSheet(QString::fromUtf8("image: url(:/icons/newregistrationicon.png);"));
         checkBox = new QCheckBox(widget);
         checkBox->setObjectName(QString::fromUtf8("checkBox"));
-        checkBox->setGeometry(QRect(630, 370, 135, 26));
-        QFont font3;
-        font3.setBold(true);
-        checkBox->setFont(font3);
-        checkBox->setStyleSheet(QString::fromUtf8("QCheckBox#checkBox{\n"
-"	background-color:rgba(255, 255, 0, 190);\n"
-"\n"
-"	color:rgba(0, 0, 0, 255);\n"
-"	border-radius:5px;\n"
+        checkBox->setGeometry(QRect(610, 370, 171, 26));
+        QFont font2;
+        font2.setPointSize(10);
+        font2.setBold(true);
+        checkBox->setFont(font2);
+        checkBox->setStyleSheet(QString::fromUtf8("QCheckBox#checkBox {\n"
+"padding-left: 10px;\n"
+"border-radius: 5px;\n"
+"color: #fefefe;\n"
 "}\n"
 "\n"
+"QCheckBox#checkBox:hover {\n"
+"	color: rgb(241, 182, 88);\n"
+"}\n"
 "\n"
-"QCheckBox#checkBox:hover{\n"
-"	background-color:rgb(246, 120, 29);\n"
-"\n"
+"QCheckBox#checkBox:pressed {\n"
+"	color: rgb(85, 255, 255);\n"
 "}"));
+        horizontalLayoutWidget = new QWidget(widget);
+        horizontalLayoutWidget->setObjectName(QString::fromUtf8("horizontalLayoutWidget"));
+        horizontalLayoutWidget->setGeometry(QRect(340, 490, 261, 80));
+        horizontalLayout = new QHBoxLayout(horizontalLayoutWidget);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        Back_pushButton = new QPushButton(horizontalLayoutWidget);
+        Back_pushButton->setObjectName(QString::fromUtf8("Back_pushButton"));
+        QFont font3;
+        font3.setPointSize(15);
+        font3.setBold(true);
+        Back_pushButton->setFont(font3);
+        Back_pushButton->setStyleSheet(QString::fromUtf8("QPushButton#Back_pushButton {\n"
+"color: white;\n"
+"border-radius: 5px;\n"
+"Background: #933;\n"
+"color: #fefefe;\n"
+"}\n"
+"\n"
+"QPushButton#Back_pushButton:hover {\n"
+"	background-color: rgb(42, 46, 52);\n"
+"border-style: solid;\n"
+" border-width: 1px;\n"
+"	color: rgb(241, 182, 88);\n"
+"}\n"
+"\n"
+"QPushButton#Back_pushButton:pressed {\n"
+"border-style: solid;\n"
+"border-width: 1px;\n"
+"	background-color: rgb(25, 69, 105);\n"
+"	color: rgb(85, 255, 255);\n"
+"}"));
+
+        horizontalLayout->addWidget(Back_pushButton);
+
+        Submit_pushButton = new QPushButton(horizontalLayoutWidget);
+        Submit_pushButton->setObjectName(QString::fromUtf8("Submit_pushButton"));
+        Submit_pushButton->setFont(font3);
+        Submit_pushButton->setStyleSheet(QString::fromUtf8("QPushButton#Submit_pushButton {\n"
+"color: white;\n"
+"border-radius: 5px;\n"
+"Background: #393;\n"
+"color: #fefefe;\n"
+"}\n"
+"\n"
+"QPushButton#Submit_pushButton:hover {\n"
+"	background-color: rgb(42, 46, 52);\n"
+"border-style: solid;\n"
+" border-width: 1px;\n"
+"	color: rgb(241, 182, 88);\n"
+"}\n"
+"\n"
+"QPushButton#Submit_pushButton:pressed {\n"
+"border-style: solid;\n"
+"border-width: 1px;\n"
+"	background-color: rgb(25, 69, 105);\n"
+"	color: rgb(85, 255, 255);\n"
+"}"));
+
+        horizontalLayout->addWidget(Submit_pushButton);
+
 
         retranslateUi(Register);
 
@@ -226,10 +324,10 @@ public:
         Password_label->setText(QCoreApplication::translate("Register", "Password: ", nullptr));
         ConfrimPassword_label->setText(QCoreApplication::translate("Register", "Confirm password: ", nullptr));
         PhoneNumber_label->setText(QCoreApplication::translate("Register", "Phone number: ", nullptr));
-        Submit_pushButton->setText(QCoreApplication::translate("Register", "Submit", nullptr));
-        Back_pushButton->setText(QCoreApplication::translate("Register", "Back", nullptr));
         label->setText(QString());
         checkBox->setText(QCoreApplication::translate("Register", "Show password", nullptr));
+        Back_pushButton->setText(QCoreApplication::translate("Register", "Back", nullptr));
+        Submit_pushButton->setText(QCoreApplication::translate("Register", "Submit", nullptr));
     } // retranslateUi
 
 };
